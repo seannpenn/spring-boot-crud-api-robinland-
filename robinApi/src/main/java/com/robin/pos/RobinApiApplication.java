ba4982129abad2adcf587d9d496ac1e9398d2776
seannpenn <pinotesean6@gmail.com>
2024-08-01 23:57:30 +0800

    add base code

---
+++ robinApi/src/main/java/com/robin/pos/RobinApiApplication.java
@@ -0,0 +1,13 @@
+package com.robin.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobinApiApplication.class, args);
+	}
+
+}
---
+++ robinApi/src/test/java/com/robin/pos/robinApi/RobinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robin.pos.robinApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
