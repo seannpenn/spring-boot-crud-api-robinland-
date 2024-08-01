a9b5f81eed3b00a4721d1a0ef7adbc83b12b054a
seannpenn <pinotesean6@gmail.com>
2024-08-01 23:58:20 +0800

    add auditable model

---
+++ robinApi/src/main/java/com/robin/pos/model/Auditable.java
@@ -0,0 +1,35 @@
+package com.robin.pos.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Setter
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class Auditable {
+    @CreatedBy
+    @Column(updatable = false)
+    protected String createdBy;
+
+    @CreatedDate
+    @Column(updatable = false)
+    protected LocalDateTime createdDate;
+
+    @LastModifiedBy
+    protected String updatedBy;
+
+    @LastModifiedDate
+    protected LocalDateTime updatedDate;
+
+}
